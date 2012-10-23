14856f8dabc83caf5c1bfb2e85210a8f0375fcfa
Shaun Guth <shaun.guth@gmail.com>
2012-10-23 00:29:08 +0000

    Support for _ context variable (last result)

--- plugins/math.js
+++ plugins/math.js
@@ -1,13 +1,16 @@
 var pipsqueek = require('../lib/pipsqueek.js').pipsqueek;
 var TapDigit = require('../lib/TapDigit.js');
 
-var evaluator = new TapDigit.Evaluator();
+var context = new TapDigit.Context();
+
+var evaluator = new TapDigit.Evaluator(context);
 
 pipsqueek.on('math', function (message) {
     try {
         result = evaluator.evaluate(message.cstring);
         message.respond(message.cstring + ' = ' + result);
-    } catch (e) {
+    }
+    catch (e) {
         message.reply(e);
     }
 });
