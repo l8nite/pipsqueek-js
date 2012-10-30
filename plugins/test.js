d3d5e35e310ede8204d91a5955f31a2f7922a39a
Shaun Guth <shaun.guth@gmail.com>
2012-10-30 10:48:47 +0000

    use new event namespacing

--- plugins/test.js
+++ plugins/test.js
@@ -1,5 +1,5 @@
 var pipsqueek = require('../lib/pipsqueek.js').pipsqueek;
 
-pipsqueek.on('test', function (message) {
+pipsqueek.on('test.*', function (message) {
     message.reply('Testing, testing.  1, 2, 3...');
 });
